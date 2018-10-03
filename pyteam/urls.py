c1d32ccee3a327f72fea4cca1109234d898b1bac
Sergey Gromovoy <sgromovoy@gmail.com>
2018-10-03 12:32:12 +0000

    some changes on the server

--- pyteam/urls.py
+++ pyteam/urls.py
@@ -19,7 +19,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^(?!.*(static|grappelli|admin))',
+        TemplateView.as_view(template_name='index.html')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^.', TemplateView.as_view(template_name='index.html'))
 ]
